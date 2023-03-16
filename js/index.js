995a2bdbf6d53d6c35c29c0083e15212151cc806
Natalia <77733802+Natalia-gamr@users.noreply.github.com>
2023-03-16 10:42:58 +0300

    Update 2023-03-16T07:42:55.534Z

--- js/index.js
+++ js/index.js
@@ -1,50 +1,55 @@
+const form = document.querySelector('.form');
+const wrapper = document.querySelector('.wrapper');
+let input = form.querySelector('input');
+let button = form.querySelector('button');
 
-
-const form = document.querySelector('.form')
-const wrapper = document.querySelector('.wrapper')
-let input = form.querySelector('input')
-let button = form.querySelector('button')
-
-const itemEl = document.createElement('div')
-wrapper.append(itemEl)
-itemEl.classList.add('repo')
+const itemEl = document.createElement('div');
+wrapper.append(itemEl);
+itemEl.classList.add('repo');
 
 async function validateForm(e) {
     if (form.name.value && form.name.value.length > 2) {
-        await data(e)
+        await data(e);
         form.reset();
         return;
     } else {
-        const errEl = document.createElement('p')
-        form.append(errEl)
-        errEl.classList.add('error')
-        errEl.innerHTML = 'Недостаточно символов'
+        const errEl = document.createElement('p');
+        errEl.classList.add('error');
+        if (form.querySelector('.error')) {
+            form.querySelector('.error').remove()
+        }
+        form.append(errEl);
+        errEl.innerHTML = 'Недостаточно символов';
+
         form.name.addEventListener('keypress', () => {
-            errEl.remove()
+            errEl.remove();
         })
     }
 }
 
 button.onclick = async (e) => {
-    e.preventDefault()
-    await validateForm(e)
+    e.preventDefault();
+    await validateForm(e);
 }
 
-let str = document.createElement('p')
-str.innerHTML = 'Репозиторий не найден'
-str.classList.add('error')
+let str = document.createElement('p');
+str.innerHTML = 'Репозитории не найдены';
+str.classList.add('error');
 
 async function data(e) {
-    e.preventDefault()
-    input = Object.fromEntries(new FormData(form))
+    e.preventDefault();
+    input = Object.fromEntries(new FormData(form));
+
     if (document.querySelectorAll('.repo__item')) {
         for (let el of document.querySelectorAll('.repo__item')) {
             el.remove();
         }
     }
+
     if (wrapper.querySelector('.error')) {
-        wrapper.querySelector('.error').remove()
+        wrapper.querySelector('.error').remove();
     }
+
     const response = await fetch(`https://api.github.com/search/repositories?q=${input.name}`);
 
     if (response.ok) {
@@ -52,30 +57,29 @@ async function data(e) {
             getData(data);
         })
     } else {
-        form.append(str)
+        form.append(str);
         return;
     }
 }
 
-
-
 function getData(data) {
-    const items = data.items
+    const items = data.items;
+
     if (items.length == 0) {
-        form.append(str)
+        form.append(str);
         return;
     } else {
         for (let i = 0; i < 10; i++) {
-            let el = items[i]
-            itemEl.append(createEl(el))
+            let el = items[i];
+            itemEl.append(createEl(el));
         }
     }
 }
 
 const createEl = (item) => {
-    const div = document.createElement('div')
-    itemEl.append(div)
-    div.classList.add('repo__item')
+    const div = document.createElement('div');
+    itemEl.append(div);
+    div.classList.add('repo__item');
 
     div.innerHTML = ` 
             <div class="repo__name">
@@ -84,11 +88,9 @@ const createEl = (item) => {
             </div>
             <div class="repo__descr">${item.description}</div>
             <div class="repo__info">
-                <div class="repo__login">author: ${item.owner.login}</div>
-                <div class="repo__lang">language: ${item.language}</div>
+                <div>author: ${item.owner.login}</div>
+                <div>language: ${item.language}</div>
             </div>
-            
         `
     return div;
 }
-
